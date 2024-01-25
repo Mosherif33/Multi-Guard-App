a2df616acbe1a9c56c66bd3f4c23351f6c3e689f
Mohamed Sherif <s.shecoo@gmail.com>
2024-01-25 12:30:01 +0200

    Added DDD

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,17 +28,17 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
+    Src\Common\Http\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
+    Src\Common\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
+    Src\Common\Exceptions\Handler::class
 );
 
 /*
